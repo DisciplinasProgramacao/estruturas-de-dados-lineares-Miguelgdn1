121d81284e42c103cfb51342113fa29bdd8f7803
Miguel Gomes do Nascimento <729466@sga.pucminas.br>
2025-10-20 11:35:50 -0300

    commit inicial

---
+++ src/Fila.java
@@ -0,0 +1,20 @@
+public class Fila {
+    private Celula primeiro, ultimo;
+
+    public Fila(){
+        primeiro = new Celula<>();
+        ultimo = primeiro;
+    }
+
+    public void inserir(int x){
+
+    }
+
+    public int remover(){
+        return 0;
+    }
+    
+    public void mostrar(){
+
+    }
+}
